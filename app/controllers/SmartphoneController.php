700efed4bba57067e0794d4ac49f37001cb26353
kelvinilliang <kelvinfilliang04@gmail.com>
2026-02-24 12:54:05 +0100

    test

---
+++ app/controllers/SmartphoneController.php
@@ -0,0 +1,28 @@
+<?php
+
+class SmartphoneController extends BaseController
+{
+    public function index()
+    {
+        /**
+         * data array informatio view pagina
+         */
+        $data = [
+            'title' => 'Overzicht Smartphones',
+        ];
+
+    
+
+
+
+    }
+
+
+
+}
+
+    
+
+
+
+
