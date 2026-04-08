79d671f4b835a1ffd7c407b2599276ab6363d551
kelvinilliang <kelvinfilliang04@gmail.com>
2026-04-08 20:55:08 +0200

    klaar

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -13,7 +13,7 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top custom-nav">
     <div class="container">
       <a class="navbar-brand" href="<?= URLROOT; ?>/homepages/index">
         <i class="bi bi-code-square"></i> MVC Basics
@@ -23,7 +23,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav  ms-auto">
           <li class="nav-item">
             <a class="nav-link" href="<?= URLROOT; ?>/SmartphoneController/index">Smartphones</a>
           </li>
